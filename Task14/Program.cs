e8354614ebaa0939e616ee63ee5fec953d5d563f
Вероника Акулова <fenek1901@gmail.com>
2022-10-06 02:08:34 +0300

    Task14 еще одно решение

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -6,20 +6,32 @@
 // 161 -> да
 
 
-bool CheckingNumder(int number, int div1, int div2)
-{
-    return number % div1 == 0 && number % div2 == 0;
-}
+// bool CheckingNumder(int number, int div1, int div2)
+// {
+//     return number % div1 == 0 && number % div2 == 0;
+// }
+// Console.WriteLine("Введите целое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// bool result = CheckingNumder(num, 7, 23);
+// if (result)
+// {
+//     Console.Write("Да");
+// }
+// else
+// {
+//     Console.Write("Нет");
+// }
+
+// Еще способ
 Console.WriteLine("Введите целое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-bool result = CheckingNumder(num, 7, 23);
-if (result)
+int number = Convert.ToInt32(Console.ReadLine());
+bool Remains(int a, int b)
 {
+    return a % b == 0;
+}
+if (Remains(number, 7) && Remains(number, 23))
+
     Console.Write("Да");
-}
 else
-{
+
     Console.Write("Нет");
-}
-
-
